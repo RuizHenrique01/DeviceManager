38aca3a6465b67ac084f696c706260a5d7ea0e37
Affonso Ruiz <affonsohenriqueruiz@gmail.com>
2025-05-01 16:12:05 -0400

    feat: Add Bootstrap and category/device list components

--- web-device-manager/src/app/app-routing.module.ts
+++ web-device-manager/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './views/main/main.component';
+import { ListCategoriesComponent } from './views/category/list-categories/list-categories.component';
+import { ListDevicesComponent } from './views/devices/list-devices/list-devices.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: 'device-manager/category',
+    pathMatch: 'full'
+  },
+  {
+    path: 'device-manager',
+    component: MainComponent,
+    children: [
+      {
+        path: 'category', // child route path
+        component: ListCategoriesComponent, // child route component that the router renders
+      },
+      {
+        path: 'device', // child route path
+        component: ListDevicesComponent, // child route component that the router renders
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- web-device-manager/src/app/app.module.ts
+++ web-device-manager/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './views/main/main.component';
+import { ListCategoriesComponent } from './views/category/list-categories/list-categories.component';
+import { ListDevicesComponent } from './views/devices/list-devices/list-devices.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ListCategoriesComponent,
+    ListDevicesComponent
   ],
   imports: [
     BrowserModule,
